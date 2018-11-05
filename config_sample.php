94e2e281937f007d9a3992ce0bc38dde6a3a3299
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2018-11-05 11:09:16 +0300

    Update config_sample.php

--- config_sample.php
+++ config_sample.php
@@ -1,10 +1,10 @@
 <? $recaptcha_override = "no"; // set to yes if you're locked out due to an invalid recaptcha site key
-$domain = ""; // domain name, no http/https prefix, e.g. website.com
+$domain = ""; // domain name, no http/https prefix, just the domain e.g. website.com
 $publisher = ""; // name for website, e.g. "My Website"
 $google_analytics_code = ""; // format is like UA-*****-*
 $google_ad_client = ""; // format is like ca-pub-********
 $server = "localhost"; // mySQL server, usually localhost
-$username = ""; // mySQL username
+$username = ""; // mySQL username, as of PHP 5.7 cannot be 'root' — you will need to create another user
 $password = ""; // mySQL password
 $database = ""; // mySQL database
 
