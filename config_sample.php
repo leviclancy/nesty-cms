17654a924316e14958c0307b9f8bdf8ad58ffc44
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2018-12-12 12:30:02 +0300

    Update config_sample.php

--- config_sample.php
+++ config_sample.php
@@ -1,11 +1,24 @@
-<? $recaptcha_override = "no"; // set to yes if you're locked out due to an invalid recaptcha site key
-$domain = ""; // domain name, no http/https prefix, just the domain e.g. website.com
-$publisher = ""; // name for website, e.g. "My Website"
-$google_analytics_code = ""; // format is like UA-*****-*
-$google_ad_client = ""; // format is like ca-pub-********
-$server = "localhost"; // mySQL server, usually localhost
-$username = ""; // mySQL username, as of PHP 5.7 cannot be 'root' — you will need to create another user
-$password = ""; // mySQL password
-$database = ""; // mySQL database
+<?
 
-$page_temp = $slug_temp = $command_temp = null; ?>
+// Override CAPTCHA
+$recaptcha_override = "no"; // set to yes if you're locked out due to an invalid recaptcha site key
+
+// Website domain
+$domain = ""; // domain name, no http/https prefix, just the domain e.g. website.com
+
+// Publisher name
+$publisher = ""; // name for website, e.g. "My Website"
+
+// Google Analytics code
+$google_analytics_code = ""; // format is like UA-*****-*
+
+// Google AdSense key
+$google_ad_client = ""; // format is like ca-pub-********
+
+// SQL information
+$server = "localhost"; // SQL server, usually localhost
+$username = ""; // SQL username, as of PHP 5.7 cannot be 'root' — you will need to create another user
+$password = ""; // SQL password
+$database = ""; // SQL database
+
+?>
