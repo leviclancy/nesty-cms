6dfe1373496817cbde38ad60b193d20ec7c2d8bb
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-07 10:14:23 -0700

    Update theme_home.php

--- theme_home.php
+++ theme_home.php
@@ -9,19 +9,19 @@ if (!(empty($description))):
 	endif;
 
 $count_temp = 0;
-$sql_temp = "SELECT page_id, created_time, header FROM $database.pages ORDER BY created_time DESC LIMIT 3";
+$sql_temp = "SELECT page_id, created_time, header FROM $database.pages ORDER BY created_time DESC LIMIT 5";
 foreach($connection_pdo->query($sql_temp) as $row):
 	if ($count_temp == 0): echo "<span class='home-header'>Recent posts</span>"; $count_temp++; endif;
 	echo body_process("{{{".$row['page_id']."}{".$row['header']."}{tile}}}");
 	endforeach;
 
 $count_temp = 0;
-$sql_temp = "SELECT media_id FROM $database.media ORDER BY datetime_original DESC LIMIT 3"; // datetime_process is alternative method of sorting
+$sql_temp = "SELECT media_id FROM $database.media ORDER BY datetime_original DESC LIMIT 5"; // datetime_process is alternative method of sorting
 foreach($connection_pdo->query($sql_temp) as $row):
 	if ($count_temp == 0): echo "<span class='home-header'>Latest images</span>"; $count_temp++; endif;
-	echo body_process("[[[".$row['media_id']."][large]]]");
+	echo body_process("[[[".$row['media_id']."][thumb]]]");
 	endforeach;
 
-echo "<span class='home-header'><a href='/schedule/'>View archive</a></span>";
+// echo "<span class='home-header'><a href='/schedule/'>View archive</a></span>";
 
 footer(); ?>
